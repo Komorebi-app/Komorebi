e02b51d53e5c6ebcb0acdc5ff0319d483608b0a8
Namania <mael.namania@gmail.com>
2026-03-18 14:52:13 +0100

    feat: play store requirements

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,12 +1,23 @@
+import java.io.FileInputStream
+import java.util.Properties
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+} else {
+    println("WARNING: key.properties not found!")
+}
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    namespace = "com.example.komorebi"
+    namespace = "fr.threeguys.komorebi"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -20,21 +31,25 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.komorebi"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        applicationId = "fr.threeguys.komorebi"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String? ?: ""
+            keyPassword = keystoreProperties["keyPassword"] as String? ?: ""
+            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+            storePassword = keystoreProperties["storePassword"] as String? ?: ""
+        }
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
---
+++ policy/règle de confidentialité.md
@@ -0,0 +1,20 @@
+# Politique de Confidentialité de Komorebi
+Dernière mise à jour : 18 03 2026
+
+Bienvenue sur Komorebi ! Nous nous engageons à protéger votre vie privée. Cette Politique de Confidentialité décrit la manière dont vos informations sont traitées par notre application.
+
+## 1. Collecte de Données Personnelles
+Komorebi ne collecte, ne stocke, ne traite et ne transmet aucune donnée personnelle de ses utilisateurs.
+
+Informations d'identification d'appareil : Nous collectons un identifiants uniques généré sur chaque appareil afin de récupérer les informations de vos livres
+
+## 3. Services Tiers
+Komorebi n'utilise aucun service tiers (comme des services d'analyse, de publicité, ou de bases de données cloud) qui pourrait collecter des informations vous concernant. L'application fonctionne de manière autonome sur votre appareil.
+
+## 4. Modifications de cette Politique de Confidentialité
+Nous nous réservons le droit de modifier cette Politique de Confidentialité à tout moment. Toute modification sera publiée sur cette page. Nous vous encourageons à consulter régulièrement cette Politique de Confidentialité pour prendre connaissance d'éventuels changements.
+
+## 5. Contactez-nous
+Si vous avez des questions concernant cette Politique de Confidentialité, vous pouvez nous contacter à l'adresse suivante :
+
+komorebi@gmail.com
