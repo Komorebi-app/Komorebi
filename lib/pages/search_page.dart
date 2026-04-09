e0a0c06e39388b1eb631008b475bed219acd7b14
Sébastien Lotten <129305476+SHACRAM@users.noreply.github.com>
2026-04-10 01:14:49 +0200

    Feat/add book form (#25)
    * Add book form
    
    * Add form to add a book manualy
    
    ---------
    
    Co-authored-by: Namania <81348116+Namania@users.noreply.github.com>

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../widgets/add_new_book_modal.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
@@ -11,9 +12,11 @@ class _SearchPageState extends State<SearchPage> {
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisSize: MainAxisSize.max,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         TextField(decoration: InputDecoration(prefixIcon: Icon(Icons.search))),
+        AddNewBookModal()
       ],
     );
   }
